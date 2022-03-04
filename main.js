03aa4c54491d2ed2e14d7a732d8a7c1946d34c98
Sundus <sundus@Nabeels-MacBook-Pro.local>
2022-03-04 01:11:17 -0800

    added new urdu language tool

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+function makeUrduPresentTense(urduGerund,englishGerund) {
+  console.log("mein "+ urduGerund + " raheenhun");
+  console.log("I am "+ englishGerund + "ing")
+}
+
+function makeUrduCommand(urduGerund,englishGerund) {
+  console.log(urduGerund + "o!");
+  console.log(englishGerund+"!")
+}
+
+function makeUrduVerbs(urduGerund,englishGerund) {
+  makeUrduPresentTense(urduGerund,englishGerund)
+  makeUrduCommand(urduGerund,englishGerund)
+  
+}
+makeUrduVerbs("seek","learn")
+
+makeUrduVerbs("seek","learn");
+makeUrduVerbs("par","read");
+makeUrduVerbs("pee","drink");
+makeUrduVerbs("jaa","go");
+makeUrduVerbs("gaa","sing");
+makeUrduVerbs("daik","watch");
+makeUrduVerbs("soch","think");
+
